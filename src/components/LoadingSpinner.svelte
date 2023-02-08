6bd4f256eca0d5101fac4a43472f4522eea80c4f
Jesús Valero <jvalerodev@gmail.com>
2023-02-08 19:56:39 -0400

    add loading spinner

---
+++ src/components/LoadingSpinner.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { SyncLoader } from 'svelte-loading-spinners';
+</script>
+
+<div
+  class="flex justify-center items-center w-screen h-screen top-0 left-0 absolute z-50"
+>
+  <SyncLoader color="#3B82F6" size="70" unit="px" />
+</div>
