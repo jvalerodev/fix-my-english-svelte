4a583a9cd9e0853af1cde39f7161586e8124fa07
Jesús Valero <jvalerodev@gmail.com>
2023-02-07 23:31:03 -0400

    add layout, form and box result

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,27 @@
   import '../app.css';
 </script>
 
-<slot />
+<div class="container mx-auto flex items-center h-screen px-10">
+  <div class="h-fit w-full">
+    <header class="text-center">
+      <h1 class="text-center font-bold text-5xl">Fix My English! 🇬🇧</h1>
+
+      <p class="py-12 text-lg text-gray-300">
+        Write <span class="font-bold text-green-500">any</span> text in English
+        that you want to be
+        <span class="font-bold text-green-500">corrected</span>!
+      </p>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</div>
+
+<style>
+  :global(body) {
+    background-color: #000;
+    color: #fff;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="md:flex justify-between md:space-x-20">
+  <form class="flex-1">
+    <textarea
+      id="message"
+      class="p-3 w-full h-80 bg-transparent text-base border border-gray-300 rounded-lg outline-none focus:border-green-500 transition-colors duration-200 resize-none"
+      placeholder="Type your text here..."
+    />
+
+    <div class="md:mb-0 my-5 text-center">
+      <button
+        class="bg-green-500 font-bold upp px-6 py-2 rounded-full hover:bg-green-600 hover:scale-105 transition duration-300"
+      >
+        Fix it!
+      </button>
+    </div>
+  </form>
+
+  <div class="flex-1 h-80 p-3 border border-gray-300 rounded-lg">
+    <h1 class="text-gray-400">Type something to get a result...</h1>
+  </div>
+</div>
